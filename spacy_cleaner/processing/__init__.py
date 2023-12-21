64f47868bcbad3aa7f6d18c75bb0bf7d4aecb66e
Cellan Hall <60790416+Ce11an@users.noreply.github.com>
2023-12-21 21:10:05 +0000

    🐛 Added missing imports. (#421)

--- spacy_cleaner/processing/__init__.py
+++ spacy_cleaner/processing/__init__.py
@@ -4,13 +4,17 @@ from spacy_cleaner.processing import evaluators, helpers, transformers
 from spacy_cleaner.processing.mutators import mutate_lemma_token
 from spacy_cleaner.processing.removers import (
     remove_email_token,
+    remove_number_token,
     remove_punctuation_token,
     remove_stopword_token,
+    remove_url_token,
 )
 from spacy_cleaner.processing.replacers import (
     replace_email_token,
+    replace_number_token,
     replace_punctuation_token,
     replace_stopword_token,
+    replace_url_token,
 )
 
 __all__ = [
@@ -18,9 +22,13 @@ __all__ = [
     "remove_stopword_token",
     "remove_punctuation_token",
     "remove_email_token",
+    "remove_url_token",
+    "remove_number_token",
     "replace_stopword_token",
     "replace_punctuation_token",
     "replace_email_token",
+    "replace_number_token",
+    "replace_url_token",
     "helpers",
     "evaluators",
     "transformers",
